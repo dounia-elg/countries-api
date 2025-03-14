e39b4a0b8a2db7e5c6d4187c736602103e14aa2a
dounia-elg <dounia.elg20@gmail.com>
2025-03-14 10:54:46 +0000

    Routes pour la gestion des pays

--- countries-api/routes/api.php
+++ countries-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\CountryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,6 +13,15 @@ Route::get('/user', function (Request $request) {
 // Routes d'authentification
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+
+
+// Routes pour les pays
+Route::apiResource('countries', CountryController::class);
+
+// Routes pour les drapeaux
+Route::post('/countries/{id}/flag', [CountryController::class, 'updateFlag']);
+Route::get('/countries/{id}/flag', [CountryController::class, 'getFlag']);
 
 
 // Routes protégées
